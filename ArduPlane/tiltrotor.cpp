516e903630a1c930a760a6ce64e7047f4ce916cc
li.hao <li.hao@sonamoo.jp>
2026-04-13 18:06:27 +0800

    save

--- ArduPlane/tiltrotor.cpp
+++ ArduPlane/tiltrotor.cpp
@@ -308,7 +308,21 @@ void Tiltrotor::continuous_update(void)
     {
         if (quadplane.rc_fwd_thr_ch == nullptr) {
             // no manual throttle control, set angle to zero
-            slew(0);
+            // slew(0);
+
+            float vectored_hover_gain = 2;
+            float des_pitch_cd = quadplane.attitude_control->get_att_target_euler_cd().y;
+            int32_t pitch_error_cd = (des_pitch_cd - quadplane.ahrs_view->pitch_sensor) * 0.5;
+            float extra_pitch = constrain_float(pitch_error_cd, -SERVO_MAX, SERVO_MAX) / SERVO_MAX;
+            float extra_sign = extra_pitch > 0?1:-1;
+            float extra_elevator = 0;
+            if (!is_zero(extra_pitch) && quadplane.in_vtol_mode()) {
+                extra_elevator = extra_sign * powf(fabsf(extra_pitch), vectored_hover_power) * SERVO_MAX;
+            }
+            tilt_motor  = extra_elevator + tilt_motor * vectored_hover_gain;
+            SRV_Channels::set_output_scaled(SRV_Channel::k_motor_tilt, tilt_motor);
+
+
         } else {
             // manual control of forward throttle up to max VTOL angle
             float settilt = 0.01f * quadplane.forward_throttle_pct();
--- ENV.md
+++ ENV.md
@@ -1,2 +1,19 @@
 
 git submodule update --init --recursive
+
+
+git submodule sync
+git submodule update --init --recursive --force
+
+
+float vectored_hover_gain = 2;
+float des_pitch_cd = quadplane.attitude_control->get_att_target_euler_cd().y;
+int32_t pitch_error_cd = (des_pitch_cd - quadplane.ahrs_view->pitch_sensor) * 0.5;
+float extra_pitch = constrain_float(pitch_error_cd, -SERVO_MAX, SERVO_MAX) / SERVO_MAX;
+float extra_sign = extra_pitch > 0?1:-1;
+float extra_elevator = 0;
+if (!is_zero(extra_pitch) && quadplane.in_vtol_mode()) {
+    extra_elevator = extra_sign * powf(fabsf(extra_pitch), vectored_hover_power) * SERVO_MAX;
+}
+tilt_motor  = extra_elevator + tilt_motor * vectored_hover_gain;
+SRV_Channels::set_output_scaled(SRV_Channel::k_motor_tilt, tilt_motor);
