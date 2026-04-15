419c9ebf9efb5ea28ead7b2d8ca074d7a39bbdad
lihao313 <7923495@qq.com>
2026-04-15 16:29:04 +0800

    save

--- ArduPlane/mode_qstabilize.cpp
+++ ArduPlane/mode_qstabilize.cpp
@@ -9,7 +9,7 @@ bool ModeQStabilize::_enter()
 
     quadplane.pilot_pitch_offset = quadplane.ahrs_view->pitch_sensor;
     float plane_aparm_pitch_limit_max = plane.aparm.pitch_limit_max;
-    plane.gcs().send_text(MAV_SEVERITY_INFO, "QStab: pilot_pitch_offset=%.1f, pitch_l_m=%.1f", (double)quadplane.pilot_pitch_offset * 0.01, (double)plane_aparm_pitch_limit_max);
+    plane.gcs().send_text(MAV_SEVERITY_INFO, "QStab: pilot_pitch_offset=%.1f, pitch_l_m=%.1f", (double)quadplane.pilot_pitch_offset, (double)plane_aparm_pitch_limit_max);
     return true;
 }
 
