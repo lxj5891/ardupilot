8adec3d8493b821b183aca5984137531f183678a
lihao313 <7923495@qq.com>
2026-04-17 15:38:20 +0800

    save

--- ArduPlane/tiltrotor.cpp
+++ ArduPlane/tiltrotor.cpp
@@ -655,18 +655,18 @@ void Tiltrotor::vectoring(void)
     // calculate the basic tilt amount from current_tilt
     float base_output = zero_out + (current_tilt * (level_out - zero_out));
 
-    static uint32_t last_debug_output_ms = 0;
-    uint32_t now = AP_HAL::millis();
-    if (now - last_debug_output_ms >= 1000) {
-        last_debug_output_ms = now;
-        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "V: t_a=%.2f z_o=%.3f f_l=%.3f l_o=%.3f b_o=%.3f c_t=%.3f",
-            (double)total_angle,
-            (double)zero_out,
-            (double)fixed_tilt_limit,
-            (double)level_out,
-            (double)base_output,
-            (double)current_tilt);
-    }
+    // static uint32_t last_debug_output_ms = 0;
+    // uint32_t now = AP_HAL::millis();
+    // if (now - last_debug_output_ms >= 1000) {
+    //     last_debug_output_ms = now;
+    //     GCS_SEND_TEXT(MAV_SEVERITY_INFO, "V: t_a=%.2f z_o=%.3f f_l=%.3f l_o=%.3f b_o=%.3f c_t=%.3f",
+    //         (double)total_angle,
+    //         (double)zero_out,
+    //         (double)fixed_tilt_limit,
+    //         (double)level_out,
+    //         (double)base_output,
+    //         (double)current_tilt);
+    // }
 
     // for testing when disarmed, apply vectored yaw in proportion to rudder stick
     // Wait TILT_DELAY_MS after disarming to allow props to spin down first.
