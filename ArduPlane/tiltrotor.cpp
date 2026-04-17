8ab7494c19e34009d6bf6122352d6b90f5006fc7
lihao313 <7923495@qq.com>
2026-04-17 20:12:54 +0800

    save

--- ArduPlane/tiltrotor.cpp
+++ ArduPlane/tiltrotor.cpp
@@ -321,7 +321,7 @@ void Tiltrotor::continuous_update(void)
         float extra_pitch = constrain_float(pitch_error_cd, -90, 90) / 90.0f;
         float extra_sign = extra_pitch > 0 ? 1: -1;
         float extra_elevator = 0;
-        if (!is_zero(extra_elevator) && quadplane.in_vtol_mode()) {
+        if (!is_zero(extra_pitch) && quadplane.in_vtol_mode()) {
             extra_elevator = extra_sign * powf(fabsf(extra_pitch), vectored_hover_power) * SERVO_MAX;
         }
         tilt_motor = extra_elevator + old_tilt_motor;
@@ -358,11 +358,11 @@ void Tiltrotor::continuous_update(void)
             float old_tilt_motor = SRV_Channels::get_output_scaled(SRV_Channel::k_scripting1);
             float vectored_hover_power = 2.5;
             float des_pitch_cd = quadplane.attitude_control->get_att_target_euler_cd().y;
+
+
             float pitch_sensor = quadplane.ahrs_view->pitch_sensor;
 
-            int32_t pitch_error_cd = (des_pitch_cd - (last_pitch_sensor - pitch_sensor)) * 0.5;
-
-            // int32_t pitch_error_cd = (last_pitch_sensor - pitch_sensor) * 0.5;
+            int32_t pitch_error_cd = (last_pitch_sensor - pitch_sensor) * 0.5;
             last_pitch_sensor = pitch_sensor;
             float extra_pitch = constrain_float(pitch_error_cd, -SERVO_MAX, SERVO_MAX) / SERVO_MAX;
             float extra_sign = extra_pitch > 0 ? 1: -1;
