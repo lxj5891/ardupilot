0083c508b4ca5f72b046f5271932237f05de5688
lihao313 <7923495@qq.com>
2026-04-20 15:05:51 +0800

    create_vtol_flap

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -175,6 +175,7 @@ void Plane::stabilize_pitch()
 
     const float pitch_out = stabilize_pitch_get_pitch_out();
     SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, pitch_out);
+    SRV_Channels::set_output_scaled(SRV_Channel::k_scripting2, pitch_out);
 }
 
 float Plane::stabilize_pitch_get_pitch_out()
